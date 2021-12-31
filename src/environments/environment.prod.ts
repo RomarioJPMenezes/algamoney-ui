c187ec0197d394066b1bf77bc519e41e5223bdda
RomarioJPMenezes <34045950+RomarioJPMenezes@users.noreply.github.com>
2021-12-31 14:20:22 +0000

    sadsad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://algamoney-apie.herokuapp.com',
-  tokenWhitelistedDomains: [ new RegExp('algamoney-apie.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('algamoney-apie.herokuapp') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
